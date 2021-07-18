441b0fb83ea917ed859f5bf4c9c991a4d991dc46
furofo <chriscellphonerepair@gmail.com>
2021-07-17 23:27:34 -0400

    added test for retrieving tab id with call back workded

--- background.js
+++ background.js
@@ -10,6 +10,20 @@ chrome.tabGroups.query({}, (obj) => {
 
 }); 
 
+chrome.tabGroups.get(987625515, (obj) => {
+    if (obj) {
+        console.log("this was succesfull too shoud return blue group"  + JSON.stringify(obj));
+
+    }
+    else {
+        console.log("this second check did not work");
+    }
+
+});
 
 /* output wiht one blue tab was did this work at least?
 background.js:5 succesfull?[{"collapsed":false,"color":"blue","id":838850965,"title":"hello","windowId":16}]*/
+
+
+/* output with two groups [{"collapsed":true,"color":"blue","id":987625515,"title":"hello","windowId":10},
+{"collapsed":true,"color":"red","id":1437632484,"title":"test2","windowId":10}]*/
