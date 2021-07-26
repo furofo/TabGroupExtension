25df13a59f50bcf4e58581e35f39e962c2a72c50
furofo <chriscellphonerepair@gmail.com>
2021-07-26 00:46:50 -0400

    cleaned up and commented code

--- popup.js
+++ popup.js
@@ -1,30 +1,19 @@
 let addRule = document.getElementById('addGroup');
 
+//this adds a property rule to local storage that we will use to define search urls for the tab groups
 function setRule() {
     chrome.storage.sync.set({rule: "google.com"}, () => {
       console.log("value is set to", "google .com" );
     });
   }
 
-
+// when button clicked for right now reads rule but functionality built to set rule//
   addRule.addEventListener("click", async() => {
     chrome.storage.sync.get(['rule'], (result) => {
         addRule.style.backgroundColor = "green";
         console.log("yeah baby this gets the rule set earlire", result.rule);
+        // setRule();
 
     });
 });
 
-  /*
-addRule.addEventListener("click", async() => {
-    chrome.storage.sync.get('rule').then((result) => {
-        addRule.style.backgroundColor = "green";
-        console.log("yeah baby this gets the rule set earlire", result);
-
-    });
-    
-    setRule();
-}); */
-
-// The body of this function will be executed as a content script inside the
-  // current page
