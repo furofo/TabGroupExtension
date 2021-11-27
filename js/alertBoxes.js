287ba3274aa629892db7d1e2afda332ec138990a
furofo <chriscellphonerepair@gmail.com>
2021-11-26 23:02:23 -0500

    organized code further added logic for confirm and go back

--- js/alertBoxes.js
+++ js/alertBoxes.js
@@ -8,23 +8,48 @@ const ModalWindow = {
   
     },
     getHtmlTemplate(modalOptions) {
+      if(modalOptions.buttons){
         return`
-       <div class="modal__overlay">
-        <div class="modal__window">
-          <div class="modal__titlebar">
-            <span class="modal__title">${modalOptions.title}</span>
-            <button class = "modal__close material-icons">close</button>
-          </div>
-          <div class="modal__content">
-           ${modalOptions.content}
-          </div>
-        </div>
-      </div>`;
+        <div class="modal__overlay">
+         <div class="modal__window">
+           <div class="modal__titlebar">
+             <span class="modal__title">${modalOptions.title}</span>
+             <button class = "modal__close material-icons">close</button>
+           </div>
+           <div class="modal__content">
+            ${modalOptions.content}
+           </div>
+           <div class = "modal__buttons">
+           <button class = "modal__goback__button">Go Back</button>
+            <button class = "modal__confirm__button">Confirm</button>
+           </div>
+          
+         </div>
+       </div>`;
+
+      }
+
+      else{
+        return`
+        <div class="modal__overlay">
+         <div class="modal__window">
+           <div class="modal__titlebar">
+             <span class="modal__title">${modalOptions.title}</span>
+             <button class = "modal__close material-icons">close</button>
+           </div>
+           <div class="modal__content">
+            ${modalOptions.content}
+           </div>
+         </div>
+       </div>`;
+      }
+       
     },
     openModal(modalOptions = {}) {
         modalOptions = Object.assign({
             title: 'Modal Title',
-            content: 'Modal Content'
+            content: 'Modal Content',
+            buttons: true,
         }, modalOptions)
   
   const modalTemplate = this.getHtmlTemplate(modalOptions);
