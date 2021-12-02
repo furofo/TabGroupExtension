f7cc7308f19a988419f6335bc400549df2553751
furofo <chriscellphonerepair@gmail.com>
2021-12-02 02:58:14 -0500

    added logic to look at tab names instead of id to match sorting

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ function chromeStorageGet(result) {
 // variables to store objects for groups 1, 2, 3, and put in array to loop through later
 // for any associated tab group ids
 const groupIDArray = [{ GROUP1: {} }, { GROUP2: {} }, { GROUP3: {} }];
-// when a tab group is completly removed this fires
+// when a tab group is completly removed this fires and clears out local groupIdArSrays TABGROUP Property
 chrome.tabGroups.onRemoved.addListener((tabGroup) => {
   for (let i = 0; i < groupIDArray.length; i += 1) {
     const group = `GROUP${String(i + 1)}`; 
@@ -24,6 +24,12 @@ chrome.tabGroups.onRemoved.addListener((tabGroup) => {
 // listener that can tell if tab changes and new html page loads or if new tab is opened
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // only exectue if tabs are fully loaded
+
+
+  chrome.tabGroups.query({}).then(
+    (tabGroupObj) => {
+      //debugger;
+  
   if (changeInfo.status === 'complete' && tab.status === 'complete'
   && tab.url !== undefined) {
     // this is promise chain of chrome.storage.get instead of callback
@@ -33,29 +39,52 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       for (let i = 0; i < result.TABGROUPS.length; i += 1) {
         const group = `GROUP${String(i + 1)}`;
         if (Object.prototype.hasOwnProperty.call(result.TABGROUPS[i], group)) {
+         
           const searchTerm = result.TABGROUPS[i][group].URL;
+          console.log("this is search term \n", searchTerm);
           if (url.includes(searchTerm)) {
-            // if tab has a group id already do nothing, if it doesn't see if the locla
+                  console.log('this is tab group object! \n', tabGroupObj);
+                  console.log("this is result \n", result);
+                  let match = false;
+                  let testid;
+                  for (let j = 0; j < tabGroupObj.length; j++) {
+                    if(result.TABGROUPS[i][group] && tab.groupId === -1 && tabGroupObj[j].title === result.TABGROUPS[i][group].NAME ) {
+                      match = true;
+                      chrome.tabs.group({
+                        tabIds: tabId,
+                        groupId: tabGroupObj[j].id,
+                      });
+                    }
+                  }
+                  if(!match) {
+                    // if tab doesn't have a group id already and no other tabs following that same
+                    // group rule, make a new tab group and update localvariable groupIDArray with a
+                    // property TABGROUP that holds that id
+                    chrome.tabs.group({ tabIds: tabId }).then((id) => {
+                      chrome.tabGroups.update(id, {
+                        title: result.TABGROUPS[i][group].NAME,
+                        color: result.TABGROUPS[i][group].COLOR,
+                      });
+                      groupIDArray[i][group].TABGROUP = id;
+                    });
+                  }
+
+
+
+                
+            
+             // if tab has a group id already do nothing, if it doesn't see if the locla
             // object for same group numberhas a property callled tab group, this stores the
             // tab group ID and means already a tab that, that follows same group rule and matched,
             // if so then group it to that existing tab group id
-            if (tab.groupId === -1 && Object.prototype.hasOwnProperty.call(groupIDArray[i][group], 'TABGROUP')) {
-              chrome.tabs.group({
-                tabIds: tabId,
-                groupId: groupIDArray[i][group].TABGROUP,
-              });
-            } else {
-              // if tab doesn't have a group id already and no other tabs following that same
-              // group rule, make a new tab group and update localvariable groupIDArray with a
-              // property TABGROUP that holds that id
-              chrome.tabs.group({ tabIds: tabId }).then((id) => {
-                chrome.tabGroups.update(id, {
-                  title: result.TABGROUPS[i][group].NAME,
-                  color: result.TABGROUPS[i][group].COLOR,
-                });
-                groupIDArray[i][group].TABGROUP = id;
-              });
-            }
+            // if (tab.groupId === -1 && Object.prototype.hasOwnProperty.call(groupIDArray[i][group], 'TABGROUP')) {
+           
+              
+            //   chrome.tabs.group({
+            //     tabIds: tabId,
+            //     groupId: groupIDArray[i][group].TABGROUP,
+            //   });
+            // } 
           }
         }
       }
@@ -63,9 +92,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     });
   }
-});
+    }
+    );
 
-let results = {
-  total: 0,
-  bad: 0
-};
+
+
+
+});
--- readme.md
+++ readme.md
@@ -3,13 +3,13 @@
 
 To use this extension, make sure you are chromium browser. Download from https://chrome.google.com/webstore/detail/auto-tab-groups/nicjeeimgboiijpfcgfkbemiclbhfnio?hl=en . Then to make it easier to use extension click jigsaw button in top right corner beside profile pic. Find the extension there,  then click the pin icon to create an icon you click on by the browser
 
-# Youtube Guide on How to use Tool
+# Youtube Guide On How To Use Tool
 https://www.youtube.com/watch?v=aLIIAZAC4bo
 
 
-** Extension Overview **
+## Extension Overview 
 
-This extensions purpose is to auto sort your chrome tabs using Google Tab Groups (detailed here)  [URL:] (https://blog.google/products/chrome/manage-tabs-with-google-chrome/ "GoogleTabGroups") and rules you set.  You can set up to 3 rules to sort your tabs by and they sort automatically each time a chrome tab is loaded or updated. 
+This extensions purpose is to auto sort your chrome tabs using Google Tab Groups (detailed here)  [ URL: ] ( https://blog.google/products/chrome/manage-tabs-with-google-chrome/ ) and rules you set.  You can set up to 3 rules to sort your tabs by and they sort automatically each time a chrome tab is loaded or updated. 
 
 The first column Name is the name of the tabgroup that tab gets sorted into. The second column, URL is the string this extension searches in the URL to determine if it goes into this particular tab group. For instance if it is set to "test2" any website you visit with test 2 in the url will be put into this tab group. The final column COLOR, is the color of this tab group that gets set.
 
@@ -18,3 +18,5 @@ When setting these values no fields can be left blank or the extension will thro
 
 
 To delete a rule or multiple rules, check one or multiple checkboxes on the left and click DELETE Group Button. This will display a confirmation message askign you if you are sure you want to delete. You can cancel this by clicking red Go Back button or the x in top right corner. To confirm delete click Green Confirm Button. This will permantly delete your rule though so keep this in mind. 
+
+## How Tool Works
