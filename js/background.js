f9bc24b8aae0932790418add3db98ff55cfa5119
furofo <chriscellphonerepair@gmail.com>
2022-05-26 03:12:16 -0400

    adde dmethod groupTabIfTabGroupExists in browser to reduce for loop nesting

--- js/background.js
+++ js/background.js
@@ -19,6 +19,26 @@ let isSearchTermInUrl =  (url, searchTerms) => {
     return false;
   }
 }
+
+//function looks through a current browswerTabGroupObject and a the a chromeStorageTabGroup Object and if the name of one of the tab group objects
+// in the the browser matches the name from chrome storage object it puts in that tab group and returns true, otherwise returns false
+function groupTabIfTabGroupExistsInBrowser(browserTabGroupObject, chromeStorageTabGroupObject, tabId) {
+  let matchingTabGroupInBrowser = false;
+  console.log("chrome storage object is " + chromeStorageTabGroupObject);
+  for (let i = 0; i < browserTabGroupObject.length; i++) {
+    if (
+      chromeStorageTabGroupObject &&
+      browserTabGroupObject[i].title === chromeStorageTabGroupObject.NAME
+    ) {
+      matchingTabGroupInBrowser = true;
+      chrome.tabs.group({
+        tabIds: tabId,
+        groupId: browserTabGroupObject[i].id,
+      });
+    }
+  }
+  return matchingTabGroupInBrowser;
+}
 // listener that can tell if a tab changes or a  new html page loads or if a new tab is opened
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // only exectue if tabs are fully loaded
@@ -32,28 +52,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
           const { url } = tab;
           if (Object.keys(result).length !== 0) {
             let ungroup = true;
+            let matchingTabGroupInBrowser = false;
             for (let i = 0; i < result.TABGROUPS.length; i += 1) {
               const group = `GROUP${String(i + 1)}`;
               if ( Object.prototype.hasOwnProperty.call(result.TABGROUPS[i], group) ) {
                 const searchTerms = result.TABGROUPS[i][group].URL;
                 if (isSearchTermInUrl(url, searchTerms)) {
-                  let matchingTabGroupInBrowser = false;
                   ungroup = false;
-                  //look through all the currrent tab groups if the name matches the crome storage tab groups name pu tin their
-                  for (let j = 0; j < tabGroupObj.length; j++) {
-                    if (
-                      result.TABGROUPS[i][group] &&
-                      // tab.groupId === -1 &&
-                      tabGroupObj[j].title === result.TABGROUPS[i][group].NAME
-                    ) {
-                      matchingTabGroupInBrowser = true;
-                      chrome.tabs.group({
-                        tabIds: tabId,
-                        groupId: tabGroupObj[j].id,
-                      });
-                    }
-                  }
-                  //if it loops through all tab groups in window and no matches found create a new tab group
+                  matchingTabGroupInBrowser = groupTabIfTabGroupExistsInBrowser(tabGroupObj, result.TABGROUPS[i][group], tabId);
                   if (!matchingTabGroupInBrowser) {
                     // if tab doesn't have a group id already and no other tabs following that same
                     // group rule, make a new tab group and update localvariable groupIDArray with a
@@ -65,12 +71,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                       });
                     });
                   }
-                }
+                  //if it loops through all tab groups in window and no matches found create a new tab group
               }
             }
-            if (ungroup) {chrome.tabs.ungroup(tabId)}
+          //if this code exectutes no matches where found on this tab id so ungroup this tab id from tabgroups if it is in one.
+          if (ungroup) {chrome.tabs.ungroup(tabId)}
           }
         }
+      }
       );
     });
   }
