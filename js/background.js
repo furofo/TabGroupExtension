b1dacbb90384e2c27f6ea6c88205a730719d216a
furofo <chriscellphonerepair@gmail.com>
2023-01-08 21:19:21 -0500

    logic to loop though all the tab groups and close them

--- js/background.js
+++ js/background.js
@@ -83,14 +83,44 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 });
-chrome.commands.onCommand.addListener((command) => {
-  console.log(`Command "${command}" triggered`);
-  // alert('KEyboard short cut ctr-shift-y used');
-  console.log("custom shortcut ctr + shift + y used")
-  chrome.tabGroups.query({}).then((browserTabGroupObject) => {
-    console.log("tab gorup object is here", browserTabGroupObject)
-    console.log("tab goup object id", browserTabGroupObject[0].id);
-    console.log("tab gorup object length", browserTabGroupObject.length);
-    chrome.tabGroups.update(browserTabGroupObject[0].id , {collapsed: true});
+// chrome.commands.onCommand.addListener((command) => {
+//   console.log(`Command "${command}" triggered`);
+//   // alert('KEyboard short cut ctr-shift-y used');
+//   console.log("custom shortcut ctr + shift + y used")
+//   chrome.tabGroups.query({}).then((browserTabGroupObject) => {
+//     if(browserTabGroupObject == undefined || browserTabGroupObject.length === 0) {
+//       console.log("No tab groups found")
+//     }
+//     else {
+//     console.log("tab gorup object is here", browserTabGroupObject)
+//     console.log("tab goup object id", browserTabGroupObject[0].id);
+//     console.log("tab gorup object length", browserTabGroupObject.length);
+//     chrome.tabGroups.update(browserTabGroupObject[0].id , {collapsed: true});
+//     }
+//   });
+// });
+
+//functoin that uses chrome commands api to collapse all tab groups when ctrl + shift + y is pressed 
+function closeTabGroupsWhenCtrlShiftY() {
+  chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command "${command}" triggered`);
+    // alert('KEyboard short cut ctr-shift-y used');
+    console.log("custom shortcut ctr + shift + y used")
+    chrome.tabGroups.query({}).then((browserTabGroupObject) => {
+      console.log("tab gorup object is here", browserTabGroupObject)
+      console.log("tab goup object id", browserTabGroupObject[0].id);
+      console.log("tab gorup object length", browserTabGroupObject.length);
+    //  if(browserTabGroupObject != undefined && browserTabGroupObject.length != 0) {
+    //   chrome.tabGroups.update(browserTabGroupObject[0].id , {collapsed: true});
+    //   }
+    if (typeof browserTabGroupObject !== "undefined" && browserTabGroupObject.length > 0) {
+      for (let i = 0; i < browserTabGroupObject.length; i++) {
+        chrome.tabGroups.update(browserTabGroupObject[i].id, { collapsed: true });
+      }
+    }
+    });
   });
-});
+}
+
+//call my created functions
+closeTabGroupsWhenCtrlShiftY();
