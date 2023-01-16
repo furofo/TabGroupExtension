3499d443f562922463f09e9e8def8ad32d8d3d63
furofo <chriscellphonerepair@gmail.com>
2023-01-16 00:50:25 -0500

    mapped added rule to add button

--- js/globalValAndFunction.js
+++ js/globalValAndFunction.js
@@ -58,111 +58,7 @@ const determineClickHandlerInB = (elemArr, elemToMatch) => {
 };
 
 
-let createRuleElement = function() {
-  let rulesContainerElement = document.querySelector(".rules-container");
 
-
-    let centerRuleDiv = document.createElement("div");
-    centerRuleDiv.classList.add("center");
-    centerRuleDiv.classList.add("rule");
-   
-
-    let containerLabel = document.createElement("label");
-    containerLabel.classList.add("container");
-    let containerLabelChildInput = document.createElement("input");
-    containerLabelChildInput.type = "checkbox";
-    let containerLabelSpanChild = document.createElement("span");
-    containerLabelSpanChild.classList.add("checkmark");
-    containerLabel.appendChild(containerLabelChildInput);
-    containerLabel.appendChild(containerLabelSpanChild);
-    centerRuleDiv.appendChild(containerLabel);
-    let nameContentDiv = document.createElement("div");
-    nameContentDiv.classList.add("rule-content");
-    nameContentDiv.classList.add("name-content");
-    let nameContentChildInput = document.createElement("input");
-    nameContentChildInput.classList.add("name");
-    nameContentChildInput.setAttribute("id", "first-name");
-    nameContentChildInput.setAttribute("value", "");
-    nameContentChildInput.value = "";
-    nameContentChildInput.disabled = true;
-    nameContentDiv.appendChild(nameContentChildInput);
-    centerRuleDiv.appendChild(nameContentDiv);
-
-    let urlContentDiv = document.createElement("div");
-    urlContentDiv.classList.add("rule-content");
-    let urlContentChildInput = document.createElement("input");
-    urlContentChildInput.classList.add("flex-center");
-    urlContentChildInput.setAttribute("id", "first-url");
-    urlContentChildInput.setAttribute("value", "");
-    urlContentChildInput.value = "";
-    urlContentChildInput.disabled = true;
-    urlContentDiv.appendChild(urlContentChildInput);
-    centerRuleDiv.appendChild(urlContentDiv);
-
-    let colorContentDiv = document.createElement("div");
-    colorContentDiv.classList.add("color-content");
-    let colorContentChildBoxDiv = document.createElement("div");
-    colorContentChildBoxDiv.classList.add("box");
-    colorContentChildBoxDiv.setAttribute("id", "first-box");
-    colorContentChildBoxDiv.setAttribute("value", "grey");
-    colorContentChildBoxDiv.value = "grey";
-    //create divs for all box colors that are not gray 
-    // let blueBox = document.createElement("div")
-    // let yellowBox = document.createElement("div")
-    // let purpleBox = document.createElement("div")
-    // let greenBox = document.createElement("div")
-    // let pinkBox = document.createElement("div")
-    // let redBox = document.createElement("div")
-    // let orangeBox = document.createElement("div")
-    // let cyanBox = document.createElement("div")
-    
-    // blueBox.classList.add("blue-box");
-    //make all color boxes divs and give them correct claass names
-    let blueBox = document.createElement("div")
-    blueBox.classList.add("blue-box");
-    let yellowBox = document.createElement("div")
-    yellowBox.classList.add("yellow-box");
-    let purpleBox = document.createElement("div")
-    purpleBox.classList.add("purple-box");
-    let greenBox = document.createElement("div")
-    greenBox.classList.add("green-box");
-    let pinkBox = document.createElement("div")
-    pinkBox.classList.add("pink-box");
-    let redBox = document.createElement("div")
-    redBox.classList.add("red-box");
-    let orangeBox = document.createElement("div")
-    orangeBox.classList.add("orange-box");
-    let cyanBox = document.createElement("div")
-    cyanBox.classList.add("cyan-box");
-    // append them as child to color Content Child Box
-    colorContentChildBoxDiv.appendChild(blueBox);
-    colorContentChildBoxDiv.appendChild(yellowBox);
-    colorContentChildBoxDiv.appendChild(purpleBox);
-    colorContentChildBoxDiv.appendChild(greenBox);
-    colorContentChildBoxDiv.appendChild(pinkBox);
-    colorContentChildBoxDiv.appendChild(redBox);
-    colorContentChildBoxDiv.appendChild(orangeBox);
-    colorContentChildBoxDiv.appendChild(cyanBox);
-    // append cololorContent ChildBox div as child to color content
-
-    colorContentDiv.appendChild(colorContentChildBoxDiv);
-    centerRuleDiv.appendChild(colorContentDiv);
-
-    let dropdown = document.createElement("div");
-    dropdown.classList.add("dropdown");
-    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.setAttribute("viewBox", "0 0 320 512");
-    svg.setAttribute("fill", "white");
-    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.setAttribute("d", "M143 256.3L7 120.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0L313 86.3c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.4 9.5-24.6 9.5-34 .1zm34 192l136-136c9.4-9.4 9.4-24.6 0-33.9l-22.6-22.6c-9.4-9.4-24.6-9.4-33.9 0L160 352.1l-96.4-96.4c-9.4-9.4-24.6-9.4-33.9 0L7 278.3c-9.4 9.4-9.4 24.6 0 33.9l136 136c9.4 9.5 24.6 9.5 34 .1z");
-
-    svg.appendChild(path);
-    dropdown.appendChild(svg);
-    centerRuleDiv.appendChild(dropdown)
-    console.log("center rule  is  ", centerRuleDiv);
-  
-}
 // // add listener to first name value and use method to set value attribute of input to what is being typed
 // document.getElementById('first-name').addEventListener('input', updateInputWhenTyped);
 // // click outside drop down to close 
@@ -178,7 +74,6 @@ let createRuleElement = function() {
 window.onload = async () => {
     let result = await chrome.storage.sync.get(['TABGROUPS']);
     console.log("Chrome Tab rules are as follows!" , result);
-    createRuleElement();
    
     if (Object.keys(result).length !== 0) {
       tabGroupsArray = [];
