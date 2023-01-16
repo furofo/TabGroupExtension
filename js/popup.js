fdc812a8b7d9f5bd4cb604b9d34caa268d48ebf7
furofo <chriscellphonerepair@gmail.com>
2023-01-16 01:37:53 -0500

    toggle drop down active when add button clicked

--- js/popup.js
+++ js/popup.js
@@ -94,7 +94,7 @@ let createRuleElement = function() {
     path.setAttribute("d", "M143 256.3L7 120.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0L313 86.3c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.4 9.5-24.6 9.5-34 .1zm34 192l136-136c9.4-9.4 9.4-24.6 0-33.9l-22.6-22.6c-9.4-9.4-24.6-9.4-33.9 0L160 352.1l-96.4-96.4c-9.4-9.4-24.6-9.4-33.9 0L7 278.3c-9.4 9.4-9.4 24.6 0 33.9l136 136c9.4 9.5 24.6 9.5 34 .1z");
     svg.appendChild(path);
     dropdown.appendChild(svg);
-    centerRuleDiv.appendChild(dropdown)
+    colorContentDiv.appendChild(dropdown);
     // console.log("center rule  is  ", centerRuleDiv);
     rulesContainerElement.appendChild(centerRuleDiv);
     return centerRuleDiv;
