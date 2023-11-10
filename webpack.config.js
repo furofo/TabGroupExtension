3067d8138e8a0f0a695e37b2984a1ec062d3c246
furofo <chriscellphonerepair@gmail.com>
2023-11-11 00:23:10 +0300

    implemented basic build using webpack
    Used weback bundler to bulde all files except for background.ts (js) into one file since chrome doesn't have good support for es6 import export syntax

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+function excludeFileAndNodeModules(fileToExclude) {
+    // Convert the specific file path to an absolute path
+    const absolutePathToExclude = path.resolve(fileToExclude);
+  
+    return (path) => {
+      // Check if the path is the node_modules folder or the specific file
+      return /node_modules/.test(path) || path === absolutePathToExclude;
+    };
+  }
+module.exports = {
+  entry: './src/popup.ts', // Your main TypeScript file
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: excludeFileAndNodeModules('C:/Users/chris/TabGroupExtension/src/background.ts')
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js', // Output file
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
