2990c42a0c62e432b30e219cd30b5bdf41dba5d7
furofo <chriscellphonerepair@gmail.com>
2023-12-04 20:38:46 +0200

    Fixed error with select button not removing delete button
    made this toggle delete group off to prevent user error

--- tests/toggleButtonText.test.ts
+++ tests/toggleButtonText.test.ts
@@ -1,43 +1,33 @@
  // Make sure to export the function in your original file
- import { toggleButtonText, deleteButton, reorderTabGroups } from "../src/globalValAndFunction"
- 
+ import { toggleButtonText, deleteButton, reorderTabGroups, deleteButtonLogic, populateTabGroupsArrayFromChromeStorage} from "../src/globalValAndFunction"
+ import {createRuleElement} from "../src/popup"
 describe('toggleButtonText', () => {
   it('should toggle button text between two strings', () => {
     // Create a mock button element
     const btn = document.createElement('button');
     btn.innerHTML = 'Text1';
-    
     const str1 = 'Text1';
     const str2 = 'Text2';
-    
     // Test the toggle
     toggleButtonText(btn, str1, str2);
     expect(btn.innerHTML).toBe('Text2');
-    
     toggleButtonText(btn, str1, str2);
     expect(btn.innerHTML).toBe('Text1');
   });
 });
-
 test('reorderTabGroups handles arrays of varying lengths correctly', () => {
   const inputShort = [{ "GROUP1": { "COLOR": "red", "NAME": "TEST", "URL":["TEST"] } }];
   const inputLong = new Array(15).fill({ "GROUP": { "COLOR": "blue" } });
-
   const resultShort = reorderTabGroups(inputShort);
   const resultLong = reorderTabGroups(inputLong);
-
   expect(resultShort.length).toBe(1);
   expect(resultLong.length).toBe(15);
 });
-
 test('reorderTabGroups removes empty objects', () => {
   const input = [{ "GROUP1": { "COLOR": "red",  "NAME": "TEST", "URL":["TEST"]} }, {}, {}];
-
   const result = reorderTabGroups(input);
-
   expect(result).toEqual([{ "GROUP1": { "COLOR": "red",  "NAME": "TEST", "URL":["TEST"] } }]);
 });
-
 test('reorderTabGroups rearranges TabGroups so that always starts with Group1 and gets rid of extra blank objects', () => {
   const groupsWithSpaceBetween = [
     {
@@ -65,11 +55,35 @@ test('reorderTabGroups rearranges TabGroups so that always starts with Group1 an
     {},
     {}
 ]
-;
-
-
   const resultSpaceBetween = reorderTabGroups(groupsWithSpaceBetween);
-  console.log(resultSpaceBetween);
   expect(resultSpaceBetween.length).toBe(2);
-
 });
+const initialTabGroups = [
+    {
+        "GROUP1": {
+            "COLOR": "red",
+            "NAME": "asdfsdaf",
+            "URL": [
+                "fsdaf"
+            ]
+        }
+    },
+    {
+        "GROUP2": {
+            "COLOR": "red",
+            "NAME": "sdf",
+            "URL": [
+                "fdsad"
+            ]
+        }
+    },
+    {
+        "GROUP3": {
+            "COLOR": "red",
+            "NAME": "sdfsda",
+            "URL": [
+                "fdssdf"
+            ]
+        }
+    }
+];
