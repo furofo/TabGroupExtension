2380352366f5a05f51a025577076b957bbba8bdb
furofo <chriscellphonerepair@gmail.com>
2023-12-15 19:03:26 +0200

    Error  Handling Logic for Improper JSON Object Type

--- options/options.js
+++ options/options.js
@@ -39,33 +39,69 @@ document.getElementById('download-btn').addEventListener('click', function () {
     });
 });
 // Function to read and process the file
-// Function to read and process the file
-// Function to read and process the file
 function handleFileSelect(evt) {
     var file = evt.target.files[0]; // Get the selected file
+    console.log("did we get toe here", file);
     // Check if the file is a JSON file
     if (file && file.name.endsWith('.json')) {
+        console.log("end with json");
         var reader = new FileReader();
         reader.onload = function (e) {
             var content = e.target.result;
             try {
+                console.log("try block reached");
                 var data = JSON.parse(content);
-                // Save the parsed data to Chrome's storage
-                chrome.storage.sync.set({ 'TABGROUPS': data }, function () {
-                    console.log('TABGROUPS data saved to Chrome storage');
-                });
+                // Validate the data against the TabGroup type
+                if (isValidTabGroup(data)) {
+                    console.log("did this even work");
+                    // Save the parsed data to Chrome's storage
+                    chrome.storage.sync.set({ 'TABGROUPS': data }, function () {
+                        console.log('TABGROUPS data saved to Chrome storage');
+                    });
+                    showTooltip(); // Assuming showTooltip is a function you've defined elsewhere
+                }
+                else {
+                    console.error("Error: Data does not match the TabGroup format. Please make sure it's a valid JSON file.\n\nExample of  a file with the correct format:\n\n                      {\n  \"group1\": {\n    \"NAME\": \"Work\",\n    \"URL\": [\"https://example.com\", \"https://worksite.com\"],\n    \"COLOR\": \"blue\"\n  },\n  \n                      \"group2\": {\n    \"NAME\": \"Leisure\",\n    \"URL\": [\"https://youtube.com\", \"https://reddit.com\"],\n    \"COLOR\": \"red\"\n  }\n}");
+                }
             }
             catch (error) {
                 console.error("Error parsing JSON:", error);
+                if (error instanceof SyntaxError) {
+                    console.error("Syntax error in file ".concat(file.name, ": ").concat(error.message));
+                    // Inform the user about the correct format
+                    alert("Failed to load file ".concat(file.name, ". Please make sure it's a valid JSON file.\n\nExample of a file with the correct format:\n{\n  \n                        \"group1\": {\n    \"NAME\": \"Work\",\n    \"URL\": [\"https://example.com\", \"https://worksite.com\"],\n    \"COLOR\": \"blue\"\n  },\n                        \n  \"group2\": {\n    \"NAME\": \"Leisure\",\n    \"URL\": [\"https://youtube.com\", \"https://reddit.com\"],\n    \"COLOR\": \"red\"\n  }\n}"));
+                }
+                else {
+                    console.error("Unexpected error processing file ".concat(file.name, ": ").concat(error.message));
+                    alert("An unexpected error occurred while processing file ".concat(file.name, ". Please try again."));
+                }
             }
         };
         reader.readAsText(file);
-        showTooltip(); // Assuming showTooltip is a function you've defined elsewhere
     }
     else {
         console.error("Error: Only .json files are accepted");
     }
 }
+function isValidTabGroup(data) {
+    // Check if data is an array
+    if (!Array.isArray(data))
+        return false;
+    for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+        var item = data_1[_i];
+        // Now, item is each object in the array
+        for (var key in item) {
+            var group = item[key];
+            if (typeof group.NAME !== 'string' ||
+                !Array.isArray(group.URL) ||
+                typeof group.COLOR !== 'string') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+// ... rest of your code ...
 // Attach event listener to the file input
 document.getElementById('file-input').addEventListener('change', handleFileSelect);
 // Function to trigger file input click
@@ -81,3 +117,4 @@ function showTooltip() {
     setTimeout(function () { return tooltip.classList.remove('show-tooltip'); }, 3000); // Hide after 3 seconds
 }
 // Example of using it in your save function
+//# sourceMappingURL=options.js.map
--- src/options.ts
+++ src/options.ts
@@ -43,34 +43,85 @@ document.getElementById('download-btn')!.addEventListener('click', function() {
     });
     
 });
-
-// Function to read and process the file
-// Function to read and process the file
-// Function to read and process the file
-function handleFileSelect(evt) {
-    const file = evt.target.files[0]; // Get the selected file
-
-    // Check if the file is a JSON file
-    if (file && file.name.endsWith('.json')) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const content = e.target!.result;
-            try {
-                const data = JSON.parse(content as string);
-                // Save the parsed data to Chrome's storage
-                chrome.storage.sync.set({ 'TABGROUPS': data }, function() {
-                    console.log('TABGROUPS data saved to Chrome storage');
-                });
-            } catch (error) {
+// TabGroup type definition
+type TabGroup = {
+    [key: string]: {
+      NAME: string;
+      URL: string[];
+      COLOR: string;
+    };
+  };
+  
+  // Function to read and process the file
+  function handleFileSelect(evt) {
+      const file = evt.target.files[0]; // Get the selected file
+      console.log("did we get toe here", file);
+      // Check if the file is a JSON file
+      if (file && file.name.endsWith('.json')) {
+        console.log("end with json");
+          const reader = new FileReader();
+          reader.onload =  function(e) {
+              const content = e.target!.result;
+              try {
+                  console.log("try block reached")
+                  const data = JSON.parse(content as string);
+                  // Validate the data against the TabGroup type
+                  if (isValidTabGroup(data)) {
+                    console.log("did this even work");
+                      // Save the parsed data to Chrome's storage
+                       chrome.storage.sync.set({ 'TABGROUPS': data }, function() {
+                          console.log('TABGROUPS data saved to Chrome storage');
+                      });
+                      
+                      showTooltip(); // Assuming showTooltip is a function you've defined elsewhere
+                  } else {
+                      console.error(`Error: Data does not match the TabGroup format. Please make sure it's a valid JSON file.\n\nExample of  a file with the correct format:\n
+                      {\n  "group1": {\n    "NAME": "Work",\n    "URL": ["https://example.com", "https://worksite.com"],\n    "COLOR": "blue"\n  },\n  
+                      "group2": {\n    "NAME": "Leisure",\n    "URL": ["https://youtube.com", "https://reddit.com"],\n    "COLOR": "red"\n  }\n}`);
+                  }
+              } catch (error) {
                 console.error("Error parsing JSON:", error);
+            
+                if (error instanceof SyntaxError) {
+                    console.error(`Syntax error in file ${file.name}: ${error.message}`);
+                    // Inform the user about the correct format
+                    alert(`Failed to load file ${file.name}. Please make sure it's a valid JSON file.\n\nExample of a file with the correct format:\n{\n  
+                        "group1": {\n    "NAME": "Work",\n    "URL": ["https://example.com", "https://worksite.com"],\n    "COLOR": "blue"\n  },
+                        \n  "group2": {\n    "NAME": "Leisure",\n    "URL": ["https://youtube.com", "https://reddit.com"],\n    "COLOR": "red"\n  }\n}`);
+                } else {
+                    console.error(`Unexpected error processing file ${file.name}: ${error.message}`);
+                    alert(`An unexpected error occurred while processing file ${file.name}. Please try again.`);
+                }
+              }
+          };
+          reader.readAsText(file);
+
+      } else {
+          console.error("Error: Only .json files are accepted");
+      }
+  }
+  
+  function isValidTabGroup(data) {
+    // Check if data is an array
+    if (!Array.isArray(data)) return false;
+
+    for (const item of data) {
+        // Now, item is each object in the array
+        for (const key in item) {
+            const group = item[key];
+            if (typeof group.NAME !== 'string' || 
+                !Array.isArray(group.URL) || 
+                typeof group.COLOR !== 'string') {
+                return false;
             }
-        };
-        reader.readAsText(file);
-        showTooltip(); // Assuming showTooltip is a function you've defined elsewhere
-    } else {
-        console.error("Error: Only .json files are accepted");
+        }
     }
+    return true;
 }
+
+  
+  // ... rest of your code ...
+  
 
 
 
